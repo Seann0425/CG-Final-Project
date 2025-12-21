b94eee1a7f58ec473723358298159b7760f4b686
TTT426 <promgramtesting@gmail.com>
2025-12-21 13:02:28 +0800

    Add Front Wall and Beside Wall code

--- include/floor_helper.h
+++ include/floor_helper.h
@@ -56,3 +56,130 @@ inline Model* createWhiteFloor() {
     m->drawMode = GL_QUADS; 
     return m;
 }
+
+
+inline Model* createVerticalFloor() {
+    Model* m = new Model();
+
+    // All white plane
+    GLuint whiteTexture = createTexture("../assets/models/Wall/Reze.png");
+    // GLuint whiteTexture = createTexture("../assets/models/Wall/WallTexture.png");
+    glBindTexture(GL_TEXTURE_2D, whiteTexture);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+    m->textures.push_back(whiteTexture);
+
+    float pos[] = {
+        0.0f, 5.12f, 0.0f,
+        0.0f, 0.0f, 0.0f,
+        8.192f, 0.0f, 0.0f,
+        8.192f, 5.12f, 0.0f
+    };
+
+    float nor[] = {
+        0.0f, 0.0f, 1.0f,
+        0.0f, 0.0f, 1.0f,
+        0.0f, 0.0f, 1.0f,
+        0.0f, 0.0f, 1.0f
+    };
+
+    float tx[] = {
+        0.0f, 1.0f,
+        0.0f, 0.0f,
+        1.0f, 0.0f,
+        1.0f, 1.0f
+    };
+
+    for (int i = 0; i < 12; i++) {
+        m->positions.push_back(pos[i]);
+        m->normals.push_back(nor[i]);
+    }
+    for (int i = 0; i < 8; i++) {
+        m->texcoords.push_back(tx[i]);
+    }
+
+    m->numVertex = 4;
+    m->drawMode = GL_QUADS; 
+    return m;
+}
+
+inline Model* createVerticalSideFloor(bool left) {
+    Model* m = new Model();
+
+    // All white plane
+    GLuint whiteTexture = createTexture("../assets/models/Wall/Reze5120.png");
+    // GLuint whiteTexture = createTexture("../assets/models/Wall/WallTexture5120.png");
+    glBindTexture(GL_TEXTURE_2D, whiteTexture);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+    m->textures.push_back(whiteTexture);
+    std::vector<float> pos;
+    std::vector<float> nor;
+    std::vector<float> tx;
+
+    if (left) {
+        // vector 支援這種賦值語法！
+        pos = {
+            0.0f, 5.12f, 5.12f,
+            0.0f, 0.0f, 5.12f,
+            0.0f, 0.0f, 0.0f,
+            0.0f, 5.12f, 0.0f
+        };
+        
+        nor = {
+             1.0f, 0.0f, 0.0f,
+             1.0f, 0.0f, 0.0f,
+             1.0f, 0.0f, 0.0f,
+             1.0f, 0.0f, 0.0f
+        };
+        
+        tx = {
+            0.0f, 1.0f,
+            0.0f, 0.0f,
+            1.0f, 0.0f,
+            1.0f, 1.0f
+        };
+    }
+
+    else{
+        pos = {
+            8.192f, 5.12f, 0.0f,
+            8.192f, 0.0f, 0.0f,
+            8.192f, 0.0f, 5.12f,
+            8.192f, 5.12f, 5.12f
+        };
+        
+        nor = {
+            -1.0f, 0.0f, 0.0f,
+            -1.0f, 0.0f, 0.0f,
+            -1.0f, 0.0f, 0.0f,
+            -1.0f, 0.0f, 0.0f
+        };
+        
+        tx = {
+            0.0f, 1.0f,
+            0.0f, 0.0f,
+            1.0f, 0.0f,
+            1.0f, 1.0f
+        };
+    }
+    
+
+    for (int i = 0; i < 12; i++) {
+        m->positions.push_back(pos[i]);
+        m->normals.push_back(nor[i]);
+    }
+    for (int i = 0; i < 8; i++) {
+        m->texcoords.push_back(tx[i]);
+    }
+
+    m->numVertex = 4;
+    m->drawMode = GL_QUADS; 
+    return m;
+}
