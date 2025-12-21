cada06e3e20ebd5d203c50bd56123cceac6a8a54
TTT426 <promgramtesting@gmail.com>
2025-12-21 20:47:13 +0800

    feat: Different time will display different walls

--- src/main.cpp
+++ src/main.cpp
@@ -823,7 +823,7 @@ int main() {
     glfwPollEvents();
     // Update camera position and view
     camera.move(window);
-    EnableWall(ctx, Enabled, VerticalWallModelIndex, VerticalWallModelIndex2, VerticalWallModelIndex3);
+    EnableWall(ctx, Enabled, VerticalWallModelIndex, VerticalWallModelIndex2, VerticalWallModelIndex3, SceneTime);
     // GL_XXX_BIT can simply "OR" together to use.
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     /// TO DO Enable DepthTest
@@ -1050,7 +1050,7 @@ int main() {
         if (ImGui::Checkbox("Enable##dir", &enable)) ctx.directionLightEnable = enable ? 1 : 0;
 
         static float time = 12.0f;
-
+        SceneTime = time;
         if (ImGui::SliderFloat("Time of Day##dir", &time, 6.0f, 18.0f, "%.1f:00")) {
           time = round(time * 6.0f) / 6.0f;  // 四捨五入到 10 分鐘
 
